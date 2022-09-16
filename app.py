c8ffa31a5027191011d9e25e59ac40366f7b4a7e
franco-lamas <francomlamas@gmail.com>
2022-09-16 14:49:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from config import *
 from sqlalchemy import create_engine
 from PyOBD import *
 import pandas as pd
+from datetime import datetime
 import time
 
 PyOBD=openBYMAdata()
-
 if PyOBD.isworkingDay()==False:
     exit()
 
@@ -14,23 +14,25 @@ engine = create_engine("mysql+pymysql://{user}:{pw}@{db_host}:{port}/{db}"
                     db=quots_db,port=db_port,auth_plugin='mysql_native_password',))
 time_sleep=4
 now = "10"
+
 while now!="17":
     PyOBD=openBYMAdata()
-    PyOBD.get_bonds().drop(["expiration"],axis=1).to_sql("bonds_48hs",con=engine,if_exists="replace")
+    PyOBD.get_bonds().drop(["expiration"],axis=1).to_sql("bonds_48hs",con=engine,if_exists="replace",index=False)
     print("bonds_df")
     time.sleep(time_sleep)
     
-    PyOBD.get_bonds().to_sql("bluechips_48hs",con=engine,if_exists="replace")
+    PyOBD.get_bluechips().to_sql("bluechips_48hs",con=engine,if_exists="replace",index=False)
     print("bluechips_dF")
     time.sleep(time_sleep)
 
-    PyOBD.get_galpones().to_sql("galpones_48hs",con=engine,if_exists="replace")
+    PyOBD.get_galpones().to_sql("galpones_48hs",con=engine,if_exists="replace",index=False)
     print("galpones_dF")
     time.sleep(time_sleep)
 
-    PyOBD.get_cedears().to_sql("cedear_48hs",con=engine,if_exists="replace")
+    PyOBD.get_cedears().to_sql("cedear_48hs",con=engine,if_exists="replace",index=False)
     print("cedear_df")
     time.sleep(time_sleep)
     
 
-    now = time.strftime("%H")
+    now = datetime.now(timezone('America/Montevideo')).strftime("%H")
+    print(now)
